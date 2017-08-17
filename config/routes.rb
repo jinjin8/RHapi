bb9df1cf1c32bbfe27ba144be092b01cb9c9e310
Susha Dore <susha.dore@gmail.com>
2017-08-17 15:15:14 -0700

    add index route, terms routes and pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'terms#index'
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+  resources :terms
 end
