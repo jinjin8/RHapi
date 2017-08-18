8cd29dfb3ca8755957d5b262a7c82285064f8769
jin camou <jincamou@gmail.com>
2017-08-18 11:51:36 -0700

    add by_term scope to model & routes.rb; create method in controller; successfully pass call to search by term

--- app/controllers/clinical_vocabularies_controller.rb
+++ app/controllers/clinical_vocabularies_controller.rb
@@ -1,6 +1,16 @@
 class ClinicalVocabulariesController < ApplicationController
   def index
-    clinical_vocabulary = ClinicalVocabulary.first
+    clinical_vocabulary = ClinicalVocabulary.all.page params[:page]
+    render json: clinical_vocabulary
+  end
+
+  def show
+    clinical_vocabulary = ClinicalVocabulary.find(params[:id])
+    render json: clinical_vocabulary
+  end
+
+  def by_term
+    clinical_vocabulary = ClinicalVocabulary.by_term(params[:Term])
     render json: clinical_vocabulary
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'clinical_vocabularies#index'
-  concern :paginatable do
-    get '(page/:page)', action: :index, on: :collection, as: ''
+  # concern :paginatable do
+  #   get 'page/:page', action: :index, on: :collection, as: ''
+  # end
+  resources :clinical_vocabularies do
+    get 'page/:page', action: :index, on: :collection, as: ''
+    collection do
+      get :by_term
+    end
   end
-  resources :clinical_vocabularies
 end
