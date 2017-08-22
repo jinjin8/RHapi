ece4ce30092d367a3c561c29e8e952525864b68e
Susha Dore <susha.dore@gmail.com>
2017-08-22 16:03:49 -0700

    DRY model code

--- app/models/clinical_vocabulary.rb
+++ app/models/clinical_vocabulary.rb
@@ -1,11 +1,6 @@
 class ClinicalVocabulary < ActiveRecord::Base
   #Adjusts for rails naming conventions using AR method
   self.table_name = "KnowledgeBase.dbo.v_ClinicalVocabularies"
-  #Scope to create a by_term endpoint
-  #http://localhost:3000/clinical_vocabularies/by_term?Term=diabetes%20mellitus
-  scope :by_term, -> (term, source_language, output_language) { self.get_problem(term, source_language, output_language)}
-  #Method to implement custom stored procedure
-  def self.get_problem(term, source_language, output_language)
-    self.execute_procedure("sp_GetProblem", term, source_language, output_language)
-  end
+  #Scope to implement custom stored procedure
+  scope :by_term, -> (term, source_language, output_language) { self.execute_procedure("sp_GetProblem", term, source_language, output_language)}
 end
