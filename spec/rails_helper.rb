bb7c5a0124f8cd92820b23f735529d1af4b85993
jin camou <jincamou@gmail.com>
2017-08-31 16:42:13 -0700

    testing env in place; WIP

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_girl'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'support/factory_girl'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
--- spec/requests/get_by_term_spec.rb
+++ spec/requests/get_by_term_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe "get by term route", :type => :request do
   clinical_vocabulary = FactoryGirl.create(:clinical_vocabulary)
 
-  it 'returns successfull route response' do
+  it 'returns successful route response' do
     get "/clinical_vocabulary/by_term?term=#{clinical_vocabulary.term}&#{clinical_vocabulary.source_lang}&#{clinical_vocabulary.output_lang}"
-    expect(response).to have_http_status(200)
+    expect(response).to have_https_status(200)
   end
 end
