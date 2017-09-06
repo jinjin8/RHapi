2b1531e6c4d30c269bf0f420178b28fd81e14787
Susha Dore <susha.dore@gmail.com>
2017-09-06 15:30:06 -0700

    simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  # root to: 'charts#index'
-
-  resources :problems do
-    get 'page/:page', action: :index, on: :collection, as: ''
-    collection do
-      get :problem
-    end
-  end
-
-  resources :charts do
-    collection do
-      get :chart
-    end
-  end
+#Pluralizing routes is a Rails naming convention
+  resources :problems
+  resources :charts
 end
