802ef340727fb95d36c8b2268da5c1187915dabb
Susha Dore <susha.dore@gmail.com>
2017-09-06 15:32:25 -0700

    refactor and update chart model and controller to match updated stored procedure

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,22 +1,7 @@
 class ChartsController < ApplicationController
 
   def index
-    chart = Chart.chart(params[:domain], params[:record_id], params[:patient_id],params[:physician_id], params[:clinic_id], params[:provenance_id], params[:sort_field], params[:sort_order], params[:row_offset], params[:rows_in_subset], params[:input_language], params[:output_language])
+    chart = Chart.chart(params[:domain], params[:patient_id], params[:physician_id], params[:sort_field], params[:sort_order], params[:row_offset], params[:rows_in_subset], params[:input_language], params[:output_language])
     render json: chart
   end
-
-  def show
-    chart = Chart.find(params[:id])
-    render json: chart
-  end
-
-  def chart
-    chart = Chart.chart(params[:domain], params[:record_id], params[:patient_id],params[:physician_id], params[:clinic_id], params[:provenance_id], params[:sort_field], params[:sort_order], params[:row_offset], params[:rows_in_subset], params[:input_language],params[:output_language])
-    render json: chart
-  end
-
-  # def chart
-  #   chart = Chart.chart('ProblemLists', 1, 2, 3, 4, 5, 'Problem', 'ASC', 0, 20, 'ENG', 'ENG')
-  #   render json: chart
-  # end
 end
--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,8 +1,7 @@
 class Chart < ActiveRecord::Base
-  #Connection to second database
-  self.establish_connection :other_development
-  self.table_name = "sEMR"
-  #Scope to implement custom stored procedure
-  scope :chart, -> (domain, record_id, patient_id, physician_id, clinic_id, provenance_id, sort_field, sort_order, row_offset, rows_in_subset, input_language, output_language) {
-  self.execute_procedure("sp_GetChart", domain, record_id, patient_id, physician_id, clinic_id, provenance_id, sort_field, sort_order, row_offset, rows_in_subset, input_language, output_language)}
+  #Connection to second database, database name matches database name in database.yml file
+  establish_connection :other_development
+  #Scope to implement custom stored procedure using ActiveRecord execute_procedure function. Parameter names are consistent with Rails naming conventions. 
+  scope :chart, -> (domain, patient_id, physician_id, sort_field, sort_order, row_offset, rows_in_subset, input_language, output_language) {
+  execute_procedure("sp_GetChart", domain, patient_id, physician_id, sort_field, sort_order, row_offset, rows_in_subset, input_language, output_language)}
 end
